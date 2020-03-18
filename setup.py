fc5c424128c3dd1bce2e19d378f160019ecdcb90
shunt <shunt@twilio.com>
2020-03-18 10:19:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,12 @@ from setuptools import setup
 
 setup(
     name="soccli",
+    author="Saxon Hunt",
+    author_email="saxon.h@outlook.com",
+    url="https://github.com/noxasaxon/socless_cli",
     version="0.1.0",
     packages=["soccli"],
+    install_requires=["python-fire", "prompt_toolkit", "PyInquirer",],
     entry_points={"console_scripts": ["soccli = soccli.__main__:main"]},
 )
 
